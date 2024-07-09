414e67f2e22e741f5fb1ca6fe2a1197666e7e116
FireWolf123456 <boice.tate@gmail.com>
2024-07-09 10:41:57 -0400

    Update build.sh

--- playground/build.sh
+++ playground/build.sh
@@ -1,3 +1,12 @@
-docker-compose down -v --remove-orphans
-docker-compose rm -f -s
-docker-compose up --always-recreate-deps --remove-orphans --renew-anon-volumes --build
+
+SERVICE=$1
+
+if [[ -z "$SERVICE" ]]; then
+  docker compose down -v --remove-orphans
+  docker compose rm -f -s
+  docker compose up --always-recreate-deps --remove-orphans --renew-anon-volumes --build
+else
+  docker compose stop "$SERVICE"
+  docker compose rm "$SERVICE" -f -s
+  docker compose up "$SERVICE" --always-recreate-deps --remove-orphans --renew-anon-volumes --build
+fi
