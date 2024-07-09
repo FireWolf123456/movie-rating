a31a2ab394a6d4f6aa9be01762f75fcf49623ca6
Larry <34108925+gamethis@users.noreply.github.com>
2024-07-09 19:26:26 +0000

    updated to go container

--- .devcontainer/code_space.sh
+++ .devcontainer/code_space.sh
@@ -2,9 +2,9 @@
 DEBIAN_FRONTEND=noninteractive
 sudo apt-get update
 sudo apt-get install -y --no-install-recommends apt-utils dialog dnsutils httpie wget unzip curl jq
-GOVERSION=1.18.10
-wget https://go.dev/dl/go${GOVERSION}.linux-amd64.tar.gz
-sudo tar -C /usr/local -xzf go${GOVERSION}.linux-amd64.tar.gz
+#GOVERSION=1.18.10
+#wget https://go.dev/dl/go${GOVERSION}.linux-amd64.tar.gz
+#sudo tar -C /usr/local -xzf go${GOVERSION}.linux-amd64.tar.gz
 CURRENT_DIR=`pwd`
 cd /workspaces/movie-rating/playground/
 ./build.sh
